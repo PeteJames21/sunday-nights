93ada76a903ee76872c3476005ae285d1bf2d294
PeteJames21 <peetjames21@gmail.com>
2024-07-29 17:44:47 +0300

    Add poem card

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -1,6 +1,27 @@
+// Add a hamburger menu to the header
 const hamburger = document.querySelector('.hamburger');
 const navLinks = document.querySelector('.nav-links');
 
 hamburger.addEventListener('click', () => {
     navLinks.classList.toggle('active');
 });
+
+
+document.addEventListener('DOMContentLoaded', addClickToCards);
+
+function addClickToCards() {
+    const cards = document.querySelectorAll('.card');
+    if (cards.length > 0) {
+        cards.forEach(card => {
+            const url = card.querySelector('.hidden-link').textContent.trim();
+            if (url) {
+                card.addEventListener('click', () => {
+                    window.location.href = url;
+                });
+            }
+            else {
+                console.warn('No hidden link element found within the card.');
+            }
+        });
+    }
+}
